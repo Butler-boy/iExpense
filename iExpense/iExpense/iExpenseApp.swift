21b3ccc24aa8845d7525d576a595ae77d7809bb8
Butler-boy <jackson.barnes162@gmail.com>
2025-05-18 21:19:37 +0100

    Init

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by pina lina on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
