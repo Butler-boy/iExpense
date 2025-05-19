659a12fc764d543ae873ad9a6c2f4f27d838b05f
Butler-boy <jackson.barnes162@gmail.com>
2025-05-19 06:32:10 +0100

    Sheet for adding expense without persistence

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,13 +7,46 @@
 
 import SwiftUI
 
+
+struct ExpenseItem: Identifiable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
+
 struct ContentView: View {
-    @AppStorage("TapCount") private var tapCount = 0
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
     
     var body: some View {
-        Button("Tap Count \(tapCount)") {
-            tapCount += 1
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button (" Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
         }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
